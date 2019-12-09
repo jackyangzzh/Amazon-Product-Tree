026a8a9c825f6c178ee52debb3b92dfac29c55fa
Jack <jackyangzzh@gmail.com>
2019-12-08 21:50:07 -0600

    misleading visualization

--- data.js
+++ data.js
@@ -1,3 +1,21 @@
+
+
+var nodeTable = { nodes: [] };
+var linkTable = { links: [] };
+var nodeData;
+var linkData;
+
 d3.csv('./data/PetSupplies.csv', function (data) {
-    console.log(data);
+
+    for (var i = 0; i < data.length; i++) {
+        nodeTable.nodes.push({ name: data[i].name, group: parseInt(data[i].parent) });
+        linkTable.links.push({ source: parseInt(data[i].id), target: parseInt(data[i].parent), value: 1 });
+    }
+
+
+    nodeData = JSON.stringify(nodeTable);
+    linkData = JSON.stringify(linkTable);
+    console.log(linkData);
 });
+
+
