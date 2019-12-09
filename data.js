4f4565c4a2100e05d715417b3ee91a23db72f9c0
Jack <jackyangzzh@gmail.com>
2019-12-08 22:26:17 -0600

    customizable radius

--- data.js
+++ data.js
@@ -8,14 +8,14 @@ var linkData;
 d3.csv('./data/PetSupplies.csv', function (data) {
 
     for (var i = 0; i < data.length; i++) {
-        nodeTable.nodes.push({ name: data[i].name, group: parseInt(data[i].parent) });
+        nodeTable.nodes.push({ name: data[i].name, group: parseInt(data[i].parent), size: parseInt(Math.sqrt(data[i].productCount)) });
         linkTable.links.push({ source: parseInt(data[i].id), target: parseInt(data[i].parent), value: 1 });
     }
 
 
     nodeData = JSON.stringify(nodeTable);
     linkData = JSON.stringify(linkTable);
-    console.log(linkData);
+    console.log(nodeData);
 });
 
 
