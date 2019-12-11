a1cd5a7fd8cc0ab00e0dab419c9a39f0a6356c48
Jack Yang <jackyangzzh@gmail.com>
2019-12-11 15:48:58 -0600

    let

--- d3Interactive/tree.js
+++ d3Interactive/tree.js
@@ -1,12 +1,12 @@
 
-// var nodeTable = { nodes: [] };
-// var linkTable = { links: [] };
-// var table = { nodes: [], links: [] };
-// var nodeData;
-// var linkData;
-// var tableData;
+// let nodeTable = { nodes: [] };
+// let linkTable = { links: [] };
+// let table = { nodes: [], links: [] };
+// let nodeData;
+// let linkData;
+// let tableData;
 
-var dataTable = { nodes: [] };
+let dataTable = { nodes: [] };
 
 // Load data
 let rootNode = {};
@@ -14,6 +14,8 @@ let rootNodeName;
 let treeData;
 
 d3.csv('../data/PetSupplies.csv', function (source) {
+
+  rootNodeName = source[0].name;
 
   convertChildren(rootNode, 0);
   update(rootNode);
@@ -37,7 +39,10 @@ d3.csv('../data/PetSupplies.csv', function (source) {
   }
 
 
+  console.log(rootNodeName)
 });
+
+console.log(rootNodeName)
 
 treeData = [
   {
@@ -73,7 +78,6 @@ console.log(treeData[0]);
  treeData = [];
 
 
- console.log(rootNodeName)
  treeData.push(rootNode);
 
   console.log(treeData[0]);
@@ -81,21 +85,21 @@ console.log(treeData[0]);
 
 
 // ************** Generate the tree diagram	 *****************
-var margin = { top: 20, right: 120, bottom: 20, left: 120 },
+let margin = { top: 20, right: 120, bottom: 20, left: 120 },
   width = 1200 - margin.right - margin.left,
   height = 600 - margin.top - margin.bottom;
 
-var i = 0,
+let i = 0,
   duration = 750,
   root;
 
-var tree = d3.layout.tree()
+let tree = d3.layout.tree()
   .size([height, width]);
 
-var diagonal = d3.svg.diagonal()
+let diagonal = d3.svg.diagonal()
   .projection(function (d) { return [d.y, d.x]; });
 
-var svg = d3.select("body").append("svg")
+let svg = d3.select("body").append("svg")
   .attr("width", width + margin.right + margin.left)
   .attr("height", height + margin.top + margin.bottom)
   .append("g")
@@ -117,18 +121,18 @@ d3.select(self.frameElement).style("height", "500px");
 function update(source) {
 
   // Compute the new tree layout.
-  var nodes = tree.nodes(root).reverse();
+  let nodes = tree.nodes(root).reverse();
     links = tree.links(nodes);
 
   // Normalize for fixed-depth.
   nodes.forEach(function (d) { d.y = d.depth * 180; });
 
   // Update the nodes…
-  var node = svg.selectAll("g.node")
+  let node = svg.selectAll("g.node")
     .data(nodes, function (d) { return d.id || (d.id = ++i); })
 
   // Enter any new nodes at the parent's previous position.
-  var nodeEnter = node.enter().append("g")
+  let nodeEnter = node.enter().append("g")
     .attr("class", "node")
     .attr("transform", function (d) { return "translate(" + source.y0 + "," + source.x0 + ")"; })
     .on("click", click);
@@ -146,7 +150,7 @@ function update(source) {
     .style("fill-opacity", 1e-6);
 
   // Transition nodes to their new position.
-  var nodeUpdate = node.transition()
+  let nodeUpdate = node.transition()
     .duration(duration)
     .attr("transform", function (d) { return "translate(" + d.y + "," + d.x + ")"; });
 
@@ -158,7 +162,7 @@ function update(source) {
     .style("fill-opacity", 1);
 
   // Transition exiting nodes to the parent's new position.
-  var nodeExit = node.exit().transition()
+  let nodeExit = node.exit().transition()
     .duration(duration)
     .attr("transform", function (d) { return "translate(" + source.y + "," + source.x + ")"; })
     .remove();
@@ -170,14 +174,14 @@ function update(source) {
     .style("fill-opacity", 1e-6);
 
   // Update the links…
-  var link = svg.selectAll("path.link")
+  let link = svg.selectAll("path.link")
     .data(links, function (d) { return d.target.id; });
 
   // Enter any new links at the parent's previous position.
   link.enter().insert("path", "g")
     .attr("class", "link")
     .attr("d", function (d) {
-      var o = { x: source.x0, y: source.y0 };
+      let o = { x: source.x0, y: source.y0 };
       return diagonal({ source: o, target: o });
     });
 
@@ -190,7 +194,7 @@ function update(source) {
   link.exit().transition()
     .duration(duration)
     .attr("d", function (d) {
-      var o = { x: source.x, y: source.y };
+      let o = { x: source.x, y: source.y };
       return diagonal({ source: o, target: o });
     })
     .remove();
